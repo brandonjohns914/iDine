28e8568e670b6797584eb002e3b4f27e3dc2ef04
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2023-08-25 13:27:46 -0700

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Brandon Johns on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
